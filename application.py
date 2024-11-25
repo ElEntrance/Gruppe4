bc387874fc07f1ca75c3c259c15e24fd0be0bfc0
Skovgaardbech <72022061+Skovgaardbech@users.noreply.github.com>
2024-11-25 14:53:09 +0100

    added a file

--- application.py
+++ application.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def Homepage():
     return render_template("Homepage.html")
+
+@app.route("/Login")
+def Login():
+    return render_template("Login.html")
+
+@app.route("/Signup")
+def Signup():
+    return render_template("Signup.html")
